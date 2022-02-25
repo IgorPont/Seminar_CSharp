9eeb5d02b1e20882c2c4bd93fd82804c73bf4793
igor_pont <pontigor11@gmail.com>
2022-02-25 18:56:04 +0300

    example 002 comparison of two numbers

--- seminar001/Program.cs
+++ seminar001/Program.cs
@@ -1,7 +1,28 @@
-﻿Console.WriteLine("Введите целое число ");
+﻿/*
+
+// example 001 square number
+
+Console.WriteLine("Введите целое число ");
 int number = Convert.ToInt32(Console.ReadLine());
 
 int square = number * number;
 
 Console.Write("Квадрат данного числа равен ");
 Console.WriteLine(square);
+*/
+
+// example 002 comparison of two numbers
+
+Console.WriteLine("Введите первое число ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число ");
+int number2 = Convert.ToInt32(Console.ReadLine());
+
+if (number1 == number2 * number2)
+{
+   Console.WriteLine("Первое число является квадратом второго числа");
+}
+else
+{
+   Console.WriteLine("Первое число не является квадратом второго числа");
+}
