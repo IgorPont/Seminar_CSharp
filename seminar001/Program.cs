67b4a88a3d60b959a3c67c055e5119ba96b70d70
igor_pont <pontigor11@gmail.com>
2022-02-25 18:09:08 +0300

    example 001 square number done

---
+++ seminar001/Program.cs
@@ -0,0 +1,7 @@
+﻿Console.WriteLine("Введите целое число ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int square = number * number;
+
+Console.Write("Квадрат данного числа равен ");
+Console.WriteLine(square);
