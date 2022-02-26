4e5031827b54130280246c4d77fc83c9ac30cb7f
igor_pont <pontigor11@gmail.com>
2022-02-26 14:26:53 +0300

    Added a solution to problem 002 homework 001

--- homework001/Program.cs
+++ homework001/Program.cs
@@ -1,5 +1,5 @@
 ﻿// Задача 1: Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
-
+/*
 Console.Write("Введите первое целое число ");
 int number1 = Convert.ToInt32(Console.ReadLine());
 Console.Write("Введите второе целое число ");
@@ -7,9 +7,25 @@ int number2 = Convert.ToInt32(Console.ReadLine());
 
 if (number1 > number2)
 {
-   Console.Write("Первое число (" + number1 + ") больше второго (" + number2 + ") ");
+   Console.WriteLine("Первое число (" + number1 + ") больше второго (" + number2 + ") ");
 }
 else
 {
-   Console.Write("Второе число (" + number2 + ") больше первого (" + number1 + ") ");
+   Console.WriteLine("Второе число (" + number2 + ") больше первого (" + number1 + ") ");
 }
+*/
+
+// Задача 2: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+
+Console.Write("Введите первое целое число ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе целое число ");
+int number2 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите третье целое число ");
+int number3 = Convert.ToInt32(Console.ReadLine());
+
+int max = number1;
+if (number2 > max) max = number2;
+if (number3 > max) max = number3;
+
+Console.WriteLine("Максимальным из введенных чисел является число " + max);
