ad8904554c3a5897fa0d66b71948eb9b23337242
igor_pont <pontigor11@gmail.com>
2022-02-26 11:42:58 +0300

    example 003 days of the week

--- seminar001/Program.cs
+++ seminar001/Program.cs
@@ -1,7 +1,5 @@
-﻿/*
-
-// example 001 square number
-
+﻿// example 001 square number
+/*
 Console.WriteLine("Введите целое число ");
 int number = Convert.ToInt32(Console.ReadLine());
 
@@ -9,7 +7,6 @@ int square = number * number;
 
 Console.Write("Квадрат данного числа равен ");
 Console.WriteLine(square);
-*/
 
 // example 002 comparison of two numbers
 
@@ -25,4 +22,60 @@ if (number1 == number2 * number2)
 else
 {
    Console.WriteLine("Первое число не является квадратом второго числа");
+}
+*/
+
+// example 003 days of the week
+
+string number1 = "Mondey";
+string number2 = "Tuesday";
+string number3 = "Wednesday";
+string number4 = "Thursday";
+string number5 = "Friday";
+string number6 = "Saturday";
+string number7 = "Sunday";
+
+Console.WriteLine("Enter a sequential number of the day of the week from 1 to 7 ");
+int number_user = Convert.ToInt32(Console.ReadLine());
+
+if (number_user == 1)
+{
+   Console.Write("The number entered corresponds to ");
+   Console.WriteLine(number1);
+}
+
+if (number_user == 2)
+{
+   Console.Write("The number entered corresponds to ");
+   Console.WriteLine(number2);
+}
+
+if (number_user == 3)
+{
+   Console.Write("The number entered corresponds to ");
+   Console.WriteLine(number3);
+}
+
+if (number_user == 4)
+{
+   Console.Write("The number entered corresponds to ");
+   Console.WriteLine(number4);
+}
+
+if (number_user == 5)
+{
+   Console.Write("The number entered corresponds to ");
+   Console.WriteLine(number5);
+}
+
+if (number_user == 6)
+{
+   Console.Write("The number entered corresponds to ");
+   Console.WriteLine(number6);
+}
+
+if (number_user == 7)
+{
+   Console.Write("The number entered corresponds to ");
+   Console.WriteLine(number7);
 }
