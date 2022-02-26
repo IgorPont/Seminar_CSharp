6e9ffa6f4b6893cfa33da11da3086c0e59949c91
igor_pont <pontigor11@gmail.com>
2022-02-26 13:20:55 +0300

    example 003 days of the week bag fix

--- seminar001/Program.cs
+++ seminar001/Program.cs
@@ -27,55 +27,56 @@ else
 
 // example 003 days of the week
 
-string number1 = "Mondey";
-string number2 = "Tuesday";
-string number3 = "Wednesday";
-string number4 = "Thursday";
-string number5 = "Friday";
-string number6 = "Saturday";
-string number7 = "Sunday";
+Console.Write("Enter a sequential number of the day of the week ");
+int numberUser = Convert.ToInt32(Console.ReadLine());
 
-Console.WriteLine("Enter a sequential number of the day of the week from 1 to 7 ");
-int number_user = Convert.ToInt32(Console.ReadLine());
-
-if (number_user == 1)
+if (numberUser <= 7 && numberUser >= 1)
 {
-   Console.Write("The number entered corresponds to ");
-   Console.WriteLine(number1);
+
+   if (numberUser == 1)
+   {
+      Console.Write("The number entered corresponds to ");
+      Console.WriteLine("Monday");
+   }
+
+   if (numberUser == 2)
+   {
+      Console.Write("The number entered corresponds to ");
+      Console.WriteLine("Tuesday");
+   }
+
+   if (numberUser == 3)
+   {
+      Console.Write("The number entered corresponds to ");
+      Console.WriteLine("Wednesday");
+   }
+
+   if (numberUser == 4)
+   {
+      Console.Write("The number entered corresponds to ");
+      Console.WriteLine("Thursday");
+   }
+
+   if (numberUser == 5)
+   {
+      Console.Write("The number entered corresponds to ");
+      Console.WriteLine("Friday");
+   }
+
+   if (numberUser == 6)
+   {
+      Console.Write("The number entered corresponds to ");
+      Console.WriteLine("Saturday");
+   }
+
+   if (numberUser == 7)
+   {
+      Console.Write("The number entered corresponds to ");
+      Console.WriteLine("Sunday");
+   }
+
 }
-
-if (number_user == 2)
+else
 {
-   Console.Write("The number entered corresponds to ");
-   Console.WriteLine(number2);
-}
-
-if (number_user == 3)
-{
-   Console.Write("The number entered corresponds to ");
-   Console.WriteLine(number3);
-}
-
-if (number_user == 4)
-{
-   Console.Write("The number entered corresponds to ");
-   Console.WriteLine(number4);
-}
-
-if (number_user == 5)
-{
-   Console.Write("The number entered corresponds to ");
-   Console.WriteLine(number5);
-}
-
-if (number_user == 6)
-{
-   Console.Write("The number entered corresponds to ");
-   Console.WriteLine(number6);
-}
-
-if (number_user == 7)
-{
-   Console.Write("The number entered corresponds to ");
-   Console.WriteLine(number7);
+   Console.WriteLine("There is no such day of the week ");
 }
