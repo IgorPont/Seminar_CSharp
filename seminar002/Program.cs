b495e9eb05f86f3d3d6ca2ea868d88d5fdcfa423
igor_pont <pontigor11@gmail.com>
2022-02-28 11:19:29 +0300

    add example 002 seminar 002

--- seminar002/Program.cs
+++ seminar002/Program.cs
@@ -17,18 +17,22 @@ int FindMaxNum(int num) // метод
 
 int number = new Random().Next(10, 99);
 Console.WriteLine("Current number is " + number + " max part is " + FindMaxNum(number));
-*/
+
 
 // Задача 2. Напишите программу, которая выводит случайное трёхзначное число и удаляет вторую цифру этого числа.
 
+int FindMaxNum(int num) // метод
+{
+   int n1 = num / 100;
+   int n2 = num % 10;
 
+   int n3 = n1 * 10 + n2;
+   return n3;
+}
 
-
-
-
-
-
-
-
+int number = new Random().Next(100, 999);
+Console.WriteLine("Вот Ваше число: " + number + " вот число без второго разряда: " + FindMaxNum(number));
+*/
 
 // Задача 3. Напишите программу, которая будет принимать на вход два числа и выводить, является ли второе число кратным первому. Если второе число не кратно числу первому, то программа выводит остаток от деление.
+
