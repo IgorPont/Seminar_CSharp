d241698781de37b8b6bd5b9515f098b036f8286e
igor_pont <pontigor11@gmail.com>
2022-03-01 19:40:53 +0300

    Added a solution to problem 015 homework 002

--- homework002/Program.cs
+++ homework002/Program.cs
@@ -20,7 +20,6 @@ Console.Write("Введите трехзначное число: ");
 int NumberUser = Convert.ToInt32(Console.ReadLine());
 
 SecondNumber(NumberUser);
-*/
 
 // Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
 
@@ -44,11 +43,61 @@ void ThirdNumber(int number)
    }
 }
 
-Console.Write("Введите число: ");
+Console.Write("Введите любое число и узнаете, какая цифра третья в этом числе: ");
 int numberUser = Convert.ToInt32(Console.ReadLine());
 
 ThirdNumber(numberUser);
-
-
+*/
 
 // Задача 15: Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+
+void WeekDay(int numberWD)
+{
+   switch (numberWD)
+   {
+      case 1:
+
+         Console.WriteLine("Понедельник не является выходным днем. Попробуй еще раз, введи: dotnet run. ");
+         break;
+
+      case 2:
+
+         Console.WriteLine("Вторник не является выходным днем. Попробуй еще раз, введи: dotnet run. ");
+         break;
+
+      case 3:
+
+         Console.WriteLine("Среда не является выходным днем. Попробуй еще раз, введи: dotnet run. ");
+         break;
+
+      case 4:
+
+         Console.WriteLine("Четверг не является выходным днем. Попробуй еще раз, введи: dotnet run. ");
+         break;
+
+      case 5:
+
+         Console.WriteLine("Пятница не является выходным днем. Попробуй еще раз, введи: dotnet run. ");
+         break;
+
+      case 6:
+
+         Console.WriteLine("Ура! Суббота! И это выходной день! ");
+         break;
+
+      case 7:
+
+         Console.WriteLine("Ура! Воскресение! И это выходной день! ");
+         break;
+
+      default:
+
+         Console.WriteLine("Такого дня недели не существует. Попробуй снова, введи: dotnet run");
+         break;
+   }
+}
+
+Console.Write("Введите цифру, соответствующую дню недели и узнаешь, будет ли этот день выходным: ");
+int numberUser = Convert.ToInt32(Console.ReadLine());
+
+WeekDay(numberUser);
