6778b3a2b97f2b13baed0f48a7027853bfcc47ec
igor_pont <pontigor11@gmail.com>
2022-03-03 20:59:40 +0300

    add example 002 seminar 003

--- seminar003/Program.cs
+++ seminar003/Program.cs
@@ -1,5 +1,5 @@
 ﻿// Задача 1. Написать программу, которая принимает на вход координаты точки (X и Y), причём X ≠ 0 и Y ≠ 0 и выдаёт номер четверти плоскости, в которой находится эта точка.
-
+/*
 int GetQuart(double xPoint, double yPoint)
 {
    if (xPoint > 0 && yPoint > 0) return 1;
@@ -19,3 +19,46 @@ y = Convert.ToDouble(Console.ReadLine());
 quartNum = GetQuart(x, y);
 if (quartNum == 0) Console.WriteLine("Нельзя определить четверть ");
 else Console.WriteLine("Данная точка находится в " + quartNum + " четверти. ");
+*/
+
+// Задача 2. Написать программу, которая принимает на вход номер четверти и выводит на экран диапазон координат, доступных в этой четверти
+
+void quarterNumber(int num)
+{
+   switch (num)
+   {
+      case 1:
+         Console.WriteLine("В I четверти диапазон координа Х > 0, Y > 0");
+         break;
+
+      case 2:
+         Console.WriteLine("В II четверти диапазон координа Х < 0, Y > 0");
+         break;
+
+      case 3:
+         Console.WriteLine("В III четверти диапазон координа Х < 0, Y < 0");
+         break;
+
+      case 4:
+         Console.WriteLine("В IV четверти диапазон координа Х > 0, Y < 0");
+         break;
+
+      default:
+         Console.WriteLine("Такой четверти не существует. Попробуйте еще раз, введите номер четверти от 1 до 4.");
+         break;
+   }
+}
+
+Console.Write("Чтобы узнать диапазон координат, доступных в конкретной четверти, введите номер четверти координатной плоскости: ");
+int userNumber = Convert.ToInt32(Console.ReadLine());
+quarterNumber(userNumber);
+
+
+
+
+
+
+
+
+
+// Задача 3. Написать программу, принимающую на вход координаты двух точек и определяющую расстояние между ними
