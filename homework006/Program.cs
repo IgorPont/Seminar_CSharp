fb3f883bf0e2b09ff99a433a60760fad82707795
igor_pont <pontigor11@gmail.com>
2022-03-30 15:15:33 +0300

    add task 002 to homework 006

--- homework006/Program.cs
+++ homework006/Program.cs
@@ -1,7 +1,7 @@
 ﻿// ==== Задача 1: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
 // 0, 7, 8, -2, -2 -> 2
 // 1, -7, 567, 89, 223-> 4
-
+/*
 void CountPositiveNumbers(int command, int step)
 {
    if (command > 0)
@@ -25,14 +25,54 @@ else
 }
 Console.WriteLine();
 
-
-
-
-
-
-
+*/
 // ==== Задача 2: Пользователь вводит с клавиатуры кол-во строк и столбцов для массива. Вывести массив в виде матрицы, создав "рамку" из единиц, а внутреннюю зону матрицы заполнить нулями.
 // 3, 4 ->
 // 1 1 1 1
 // 1 0 0 1
 // 1 1 1 1
+
+int CheckNaturalNum(int number)
+{
+   while (number <= 0)
+   {
+      Console.Write($"The number of rows or columns in the array cannot be less than one. \nEnter a natural number: ");
+      number = Convert.ToInt32(Console.ReadLine());
+   }
+   return number;
+}
+
+void Frame(int row, int columns)
+{
+   if (row < 3 || columns < 3)
+   {
+      Console.WriteLine($"That's no way to get a picture. Enter more rows and columns. ");
+      Console.Write($"Enter the number of rows of the array: ");
+      int userRow = CheckNaturalNum(Convert.ToInt32(Console.ReadLine()));
+      Console.Write($"Enter the number of columns of the array: ");
+      int userColumns = CheckNaturalNum(Convert.ToInt32(Console.ReadLine()));
+      Frame(userRow, userColumns);
+   }
+   else
+   {
+      int[,] array = new int[row, columns];
+      for (int i = 0; i < array.GetLength(0); i++)
+      {
+         for (int j = 0; j < array.GetLength(1); j++)
+         {
+            if (i == 0 || j == 0 || i == array.GetLength(0) - 1 || j == array.GetLength(1) - 1)
+               array[i, j] = 1;
+            else
+               array[i, j] = 0;
+            Console.Write($"{array[i, j]}");
+         }
+         Console.WriteLine();
+      }
+   }
+}
+
+Console.Write($"Enter the number of rows of the array: ");
+int userRow = CheckNaturalNum(Convert.ToInt32(Console.ReadLine()));
+Console.Write($"Enter the number of columns of the array: ");
+int userColumns = CheckNaturalNum(Convert.ToInt32(Console.ReadLine()));
+Frame(userRow, userColumns);
